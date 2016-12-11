c4949b8c82cfd97cb23a6b5efc47fed2dc45ede0
Roland Warmerdam <rowno@webspirited.com>
2016-12-11 15:00:52 -0800

    Reduce dependency size by using the lodash method packages

--- camelcase.js
+++ camelcase.js
@@ -1,17 +1,19 @@
-var _ = require('lodash');
+var reduce = require('lodash.reduce');
+var camelCase = require('lodash.camelcase');
+var snakeCase = require('lodash.snakecase');
 
 module.exports = function (bookshelf, options) {
 
   var Model = bookshelf.Model.extend({
     parse: function (attr) {
-      return _.reduce(attr, function (record, val, key) {
-        record[_.camelCase(key)] = val;
+      return reduce(attr, function (record, val, key) {
+        record[camelCase(key)] = val;
         return record;
       }, {});
     },
     format: function (attr) {
-      return _.reduce(attr, function (record, val, key) {
-        record[_.snakeCase(key)] = val;
+      return reduce(attr, function (record, val, key) {
+        record[snakeCase(key)] = val;
         return record;
       }, {});
     }
