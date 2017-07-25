02cbbc72750816fba53997689401b5a9ae642144
Justin Stone <justin@Justins-iMac.local>
2017-07-24 22:09:59 -0400

    refactor

--- camelcase.js
+++ camelcase.js
@@ -1,16 +1,14 @@
 const camelCase = require('lodash.camelcase');
 const snakeCase = require('lodash.snakecase');
 
-const keyUpdater = { camelCase, snakeCase };
-
-const changeKey = type => attr => Object.keys(attr).reduce((updated, oldKey) => {
-  updated[keyUpdater[type](oldKey)] = attr[oldKey];
+const formatKey = formatter => attr => Object.keys(attr).reduce((updated, oldKey) => {
+  updated[formatter(oldKey)] = attr[oldKey];
   return updated;
 }, {});
 
 module.exports = (bookshelf) => {
   bookshelf.Model = bookshelf.Model.extend({
-    parse: changeKey('camelCase'),
-    format: changeKey('snakeCase')
+    parse: formatKey(camelCase),
+    format: formatKey(snakeCase)
   });
 };
